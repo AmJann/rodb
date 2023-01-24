019a087e42990285d3627892e087bfd3170c1284
amjan <amjan91@gmail.com>
2023-01-24 15:58:11 -0500

    models created, superuser added, django extenstions added

--- rod/models.py
+++ rod/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+import uuid
 
 class Artwork(models.Model):
-    name = models.CharField(max_length=100)
+    uuid = models.UUIDField(primary_key=True,unique=True, auto_created=True, default=uuid.uuid4)
+    name = models.CharField(max_length=101)
     size = models.CharField(max_length=100)
     photo_url = models.TextField()
-    description = model.TextField()
-    price = models.IntgerField()
-    type = models.CharField()
+    description = models.TextField(max_length=500)
+    price = models.CharField(max_length=20)
+    type = models.CharField(max_length=100)
+    subtype = models.CharField(max_length=100, null=True)
+    sold = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
