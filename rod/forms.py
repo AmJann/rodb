0e58c962407a0a033f872e15f9231c36ba730c64
amjan <amjan91@gmail.com>
2023-02-15 14:14:35 -0500

    add form created

--- rod/forms.py
+++ rod/forms.py
@@ -1,3 +1,19 @@
 from django import forms
 from django.forms import ModelForm
 from .models import Artwork
+
+
+class ArtworkForm(ModelForm):
+    class Meta:
+        model = Artwork
+        fields = ('name','size','description','price','type','subtype','image','sold')
+
+        widgets = {
+            'name':forms.TextInput(attrs={'class': 'form-control'}),
+            'size':forms.TextInput(attrs={'class': 'form-control'}),
+            'description':forms.TextInput(attrs={'class': 'form-control'}),
+            'price':forms.TextInput(attrs={'class': 'form-control'}),
+            'type':forms.TextInput(attrs={'class': 'form-control'}),
+            'subtype':forms.TextInput(attrs={'class': 'form-control'}),
+            'image':forms.FileInput(attrs={'class': 'form-control-file'}),
+        }
--- rod/views.py
+++ rod/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 from .models import Artwork
 from rest_framework import generics, permissions
 from .serializers import ArtworkSerializer
+
+from .forms import ArtworkForm
 
 class Artworks(generics.ListCreateAPIView):
     queryset = Artwork.objects.all()
@@ -30,3 +33,17 @@ class ArtworkDetailProtected(generics.RetrieveUpdateDestroyAPIView):
 def Artwork_list(request):
     artworks = Artwork.objects.all()
     return render(request, 'home.html', {'artworks': artworks})    
+
+def add_artwork(request):
+    submitted = False
+    if request.method == "POST":
+        form = ArtworkForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('?submitted=True')
+        else:
+            form = ArtworkForm
+            if submitted in request.GET:
+                submitted = True  
+    form = ArtworkForm
+    return render(request, 'add_artwork.html', {'form': form, 'submitted': submitted}) 
